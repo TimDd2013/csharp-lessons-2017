fc439d4a92a78f122a1dd3e8519560a4a87cbc34
TimDd2013 <s2312007@mail.zih.tu-dresden.de>
2017-11-14 16:59:59 +0100

    Debugging exercise X1 completed

---
+++ X01 Debuggin exercise 1/X01 Debuggin exercise 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                 int sum = 0;
+                 int count = 0;
+
+            while (count <= 1001) //while (count <= 1000)
+                 {
+
+                if (count % 5 == 0)
+                    if (count % 3 != 0)
+                         {
+                             sum = sum + count;
+                             Console.WriteLine(sum + " (+" + count + ")");
+
+                         }
+
+                     if (count % 3 == 0)
+                         if (count % 5 != 0)
+                         {
+                             sum = sum + count;
+                             Console.WriteLine(sum + " (+" + count + ")");
+                         }
+                count++;
+
+                 }
+
+                 // 233168 ist das Ergebnis
+
+                 // 201003 ist das korrekte Ergebnis!
+
+                 Console.WriteLine(sum);
+
+            Console.ReadLine();
+        }
+    }
+}
